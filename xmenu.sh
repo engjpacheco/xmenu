ddd0c3374b5a41ce73fed211b3ff7c46915d440f
Francisco Pacheco <francisco.pacheco@autoliv.com>
2021-12-20 05:07:59 -0600

    first commit

---
+++ xmenu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+xmenu <<EOF | sh &
+Applications
+	Web Browser	firefox
+	Image editor	gimp
+	Videos de Lia
+		Todos				lia
+		La granja de Xenon		liagranja
+		Gallina pintadita		liagallina
+
+Terminal (st)		st
+
+Shutdown		poweroff
+Reboot			Reboot
+DWM			sysact
+EOF
